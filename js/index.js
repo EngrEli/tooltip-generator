edb1e47ec660e341201edaa12f9dd6c00ba5c711
EngrEli <elizerlumilay@gmail.com>
2022-10-23 10:19:00 +0800

    Finalize tooltip-generator script

--- js/index.js
+++ js/index.js
@@ -7,9 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
   // 3. placement (top, bottom - default no value needed, left, right)
   // 4. trigger (click, hover - if no value default)
   // 5. style ( default(titleWithClose), textOnlyWithClose, textOnly)
-  // 6. title (any text or text with HTML tags, default) not done
-  generator('.testung', {
-    style: 'textOnly',
-    trigger: 'click',
+  // 6. title (any text)
+  // 7. className (custom Class Name for the tooltip container - separate class by space)
+  generator('.testing', {
+    title: '<h1>testing</h1><br/>',
+    content: '<li>test</li>',
+    className: 'class1 class1-1',
   });
 });
