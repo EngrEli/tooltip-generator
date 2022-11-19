82527f073eea3c513351fdd447490d8593b000c0
EngrEli <elizerlumilay@gmail.com>
2022-11-19 10:51:53 +0800

    Deleted package.zip

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
 import generator from './tooltipGenerator.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  // OPTIONS:
-  // 1. allowOnAnyElement (true, false(default))
-  // 2. content (any text or text with HTML tags, none(default))
-  // 3. placement (top, bottom - default no value needed, left, right)
-  // 4. trigger (click, hover - if no value default)
-  // 5. style ( default(titleWithClose), textOnlyWithClose, textOnly)
-  // 6. title (any text)
-  // 7. className (custom Class Name for the tooltip container - separate class by space)
-  generator('.testing', {
-    title: '<h1>testing</h1><br/>',
-    content: '<li>test</li>',
-    className: 'class1 class1-1',
+  generator('.your-class', {
+    className: 'class1 class2',
+    placement: 'right',
   });
 });
